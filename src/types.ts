f0c0ca305e8676893250584ca42b925b701c4078
Ade Bateman <bateman@acm.org>
2025-01-06 14:39:01 -0800

    Reformat type information to provide Typescript documentation.

--- src/types.ts
+++ src/types.ts
@@ -1,63 +1,105 @@
 /**
- * Device power source value.
+ * Enum representing different power sources.
  */
 export enum PowerSource {
   UNKNOWN = 0,
-  VIN = 1,        // Powered by VIN
-  USB_HOST = 2,   // Powered by a computer that is acting as a USB host
-  USB_ADAPTER = 3,// Powered by a USB power adapter that supports DPDM but is not a USB host
-  USB_OTG = 4,    // Powered by On-The-Go (OTG) device
-  BATTERY = 5     // Powered by battery connected to LiPo connector or Li+
+  /** Powered by VIN. */
+  VIN = 1,
+
+  /** Powered by a computer that is acting as a USB host. */
+  USB_HOST = 2,
+
+  /** Powered by a USB power adapter that supports DPDM but is not a USB host. */
+  USB_ADAPTER = 3,
+
+  /** Powered by On-The-Go (OTG) device. */
+  USB_OTG = 4,
+
+  /** Powered by battery connected to LiPo connector or Li+. */
+  BATTERY = 5
 };
 
 /**
- * Regular monitor event data.
+ * Type representing a monitor event.
  */
 export type MonitorEvent = {
-  v: number;      // Firmware version
-  beacons: {      // Temperature beacons
-    mac: string;  // MAC address
-    rssi: number; // Signal strength
-    t: number;    // Temperature (C * 100)
-    h: number;    // Humidity (% * 100)
-    b: number;    // Battery percentage (%)
-    v: number;    // Battery voltage (V * 100)
+  /** Firmware version */
+  v: number;
+  /** Temperature beacons */
+  beacons: {
+    /** MAC address */
+    mac: string;
+    /** Signal strength */
+    rssi: number;
+    /** Temperature (C * 100) */
+    t: number;
+    /** Humidity (% * 100) */
+    h: number;
+    /** Battery percentage (%) */
+    b: number;
+    /** Battery voltage (V * 100) */
+    v: number;
   }[];
-  gps: {          // GPS data
-    t: string;    // Timestamp (ISO 8601)
-    fix: number;  // Fix status (0 = no fix, 1 = fix)
+  /** GPS data */
+  gps: {
+    /** Timestamp (ISO 8601) */
+    t: string;
+    /** Fix status (0 = no fix, 1 = fix) */
+    fix: number;
     // Remaining fields are only present if fix is 1
-    sat?: number;  // Number of satellites
-    lat?: number;  // Latitude (deg * 10^7)
-    lng?: number;  // Longitude (deg * 10^7)
-    sog?: number;  // Speed over ground (knots)
-    cog?: number;  // Course over ground (deg)
-    alt?: number;  // Altitude (m)
-    pdop?: number; // Position dilution of precision
-    q?: number;    // Fix quality (0 = Invalid, 1 = GPS, 2 = DGPS)
-    q3?: number;   // 3D fix quality (1 = Nofix, 2 = 2D fix, 3 = 3D fix)
+    /** Number of satellites */
+    sat?: number;
+    /** Latitude (deg * 10^7) */
+    lat?: number;
+    /** Longitude (deg * 10^7) */
+    lng?: number;
+    /** Speed over ground (knots) */
+    sog?: number;
+    /** Course over ground (deg) */
+    cog?: number;
+    /** Altitude (m) */
+    alt?: number;
+    /** Position dilution of precision */
+    pdop?: number;
+    /** Fix quality (0 = Invalid, 1 = GPS, 2 = DGPS) */
+    q?: number;
+    /** 3D fix quality (1 = Nofix, 2 = 2D fix, 3 = 3D fix) */
+    q3?: number;
   };
   gpio: {
-    b1: number;   // Battery 1 voltage (V * 100)
-    b2: number;   // Battery 2 voltage (V * 100)
-    sp: number;   // Shore power status (0 = off, 1 = on)
-    spc: number;  // Shore power cycle (number of times shore power has been lost)
-    bp: number;   // Bilge pump status (0 = off, 1 = on)
-    bpc: number;  // Bilge pump cycle (number of times bilge pump has been activated)
-    eng: number;  // Engine status (0 = off, 1 = on)
-    engc: number; // Engine cycle (number of times engine has been started)
+    /** Battery 1 voltage (V * 100) */
+    b1: number;
+    /** Battery 2 voltage (V * 100) */
+    b2: number;
+    /** Shore power status (0 = off, 1 = on) */
+    sp: number;
+    /** Shore power cycle (number of times shore power has been lost) */
+    spc: number;
+    /** Bilge pump status (0 = off, 1 = on) */
+    bp: number;
+    /** Bilge pump cycle (number of times bilge pump has been activated) */
+    bpc: number;
+    /** Engine status (0 = off, 1 = on) */
+    eng: number;
+    /** Engine cycle (number of times engine has been started) */
+    engc: number;
   };
   sys: {
-    ps: PowerSource; // Power source
-    soc: number;  // Battery state of charge (% * 10) or -1 if disconnected
-    mem: number;  // Amount of free memory in the system in bytes
-    ss: number;   // Cellular signal strength as a percentage (% * 10)
-    sq: number;   // Cellular signal quality as a percentage (% * 10)
+    /** Power source */
+    ps: PowerSource;
+    /** Battery state of charge (% * 10) or -1 if disconnected */
+    soc: number;
+    /** Amount of free memory in the system in bytes */
+    mem: number;
+    /** Cellular signal strength as a percentage (% * 10) */
+    ss: number;
+    /** Cellular signal quality as a percentage (% * 10) */
+    sq: number;
   };
 };
 
 /**
- * AlertState is a bit field with one or more states.
+ * Enum representing different alert states.
  */
 export enum AlertState {
   ALERT_NONE = 0,
@@ -69,25 +111,38 @@ export enum AlertState {
 }
 
 /**
- * Data from alert event.
+ * Type representing an alert event.
  */
 export type AlertEvent = {
   alert: AlertState;
   gpio: {
-    b1: number;   // Battery 1 voltage (V * 100)
-    b2: number;   // Battery 2 voltage (V * 100)
-    sp: number;   // Shore power status (0 = off, 1 = on)
-    spc: number;  // Shore power cycle (number of times shore power has been lost)
-    bp: number;   // Bilge pump status (0 = off, 1 = on)
-    bpc: number;  // Bilge pump cycle (number of times bilge pump has been activated)
-    eng: number;  // Engine status (0 = off, 1 = on)
-    engc: number; // Engine cycle (number of times engine has been started)
+    /** Battery 1 voltage (V * 100) */
+    b1: number;
+    /** Battery 2 voltage (V * 100) */
+    b2: number;
+    /** Shore power status (0 = off, 1 = on) */
+    sp: number;
+    /** Shore power cycle (number of times shore power has been lost) */
+    spc: number;
+    /** Bilge pump status (0 = off, 1 = on) */
+    bp: number;
+    /** Bilge pump cycle (number of times bilge pump has been activated) */
+    bpc: number;
+    /** Engine status (0 = off, 1 = on) */
+    eng: number;
+    /** Engine cycle (number of times engine has been started) */
+    engc: number;
   };
   sys: {
-    ps: PowerSource; // Power source
-    soc: number;  // Battery state of charge (% * 10) or -1 if disconnected
-    mem: number;  // Amount of free memory in the system in bytes
-    ss: number;   // Cellular signal strength as a percentage (% * 10)
-    sq: number;   // Cellular signal quality as a percentage (% * 10)
+    /** Power source */
+    ps: PowerSource;
+    /** Battery state of charge (% * 10) or -1 if disconnected */
+    soc: number;
+    /** Amount of free memory in the system in bytes */
+    mem: number;
+    /** Cellular signal strength as a percentage (% * 10) */
+    ss: number;
+    /** Cellular signal quality as a percentage (% * 10) */
+    sq: number;
   };
 };
